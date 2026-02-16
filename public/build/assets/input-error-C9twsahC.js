1be1901a04e86e1d4ad7800b577b624578ae519f
Krishna Tiwari <kisnatwari@gmail.com>
2026-02-16 21:33:39 +0545

    new build

---
+++ public/build/assets/input-error-C9twsahC.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-C_UhYsny.js";import{c as o}from"./index-BgvHx70o.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
